9bd7746e3b4804d85e42249ee3837975598e004c
Parthavr <92426059+Parthavr@users.noreply.github.com>
2022-04-02 10:23:03 +0530

    Update Unit_Convertor.h

--- 3_Implementation/inc/Unit_Convertor.h
+++ 3_Implementation/inc/Unit_Convertor.h
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+extern void callmmfunction(float n);
+extern void callcmfunction(float n);
+extern void callinfunction(float n);
+extern void  callftfunction(float n);
+extern float cmTomm(float n);
+extern float ftToin(float n);
+extern float ftTocm(float n);
+extern float ftTomm(float n);
+extern float inToft(float n);
+extern float inToin(float n);
+extern float inTocm(float n);
+extern float inTomm(float n);
+extern float cmToft(float n);
+extern float cmToin(float n);
+extern float cmTocm(float n);
+extern float mmToft(float n);
+extern float mmToin(float n);
+extern float mmTomm(float n);
+extern float mmTocm(float n);
+extern float ftToft(float n);
