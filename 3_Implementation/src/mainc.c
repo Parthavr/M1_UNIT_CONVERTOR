27a73992a3fc516f0263f0f18ce84e9a1dd21687
Parthavr <92426059+Parthavr@users.noreply.github.com>
2022-04-02 10:24:19 +0530

    Update mainc.c

--- 3_Implementation/src/mainc.c
+++ 3_Implementation/src/mainc.c
@@ -1,1 +1,50 @@
+#include "Unit_Convert.h"
 
+int main()
+{
+    while (1)
+    {
+        int choice;
+        float n;
+
+        printf("welcome to unit converter");
+        printf("\n1.millimeter\n");
+        printf("2.centimeter\n");
+        printf("3.inch\n");
+        printf("4.feet\n");
+        printf("\n enter you choice : ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("\n enter the value : ");
+            scanf("%f", &n);
+            callmmfunction(n);
+            break;
+        case 2:
+            printf("\n enter the value :");
+            scanf("%f", &n);
+            callcmfunction(n);
+            break;
+
+        case 3:
+            printf("\n enter the value :");
+            scanf("%f", &n);
+            callinfunction(n);
+            break;
+        case 4:
+            printf("\n enter the value :");
+            scanf("%f", &n);
+            callftfunction(n);
+            break;
+        case 5:
+            exit(0);
+            break;
+        default:
+            printf("\n Wrong choice\n");
+            break;
+        }
+    }
+    return 0;
+}
