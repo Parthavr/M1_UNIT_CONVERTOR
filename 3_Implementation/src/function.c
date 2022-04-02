6c20958604d3d09c77a24dccf597b835e0f320c7
Parthavr <92426059+Parthavr@users.noreply.github.com>
2022-04-02 10:26:39 +0530

    Create function.c

---
+++ 3_Implementation/src/function.c
@@ -0,0 +1,104 @@
+float mmTomm(float n)
+{
+    
+    return n;
+}
+
+float mmTocm(float n)
+{
+    float out;
+    out = n / 10;
+    
+    return out;
+}
+
+float mmToin(float n)
+{
+    float out;
+    out = n / 25.4;
+    
+    return out;
+}
+float mmToft(float n)
+{
+    float out;
+    out = n / 305;
+    return out;
+
+}
+float cmTomm(float n)
+{
+    float out;
+    out = n * 10;
+    return out;
+}
+float cmTocm(float n)
+{
+    float out;
+    out = n * 1;
+    return n;
+}
+float cmToin(float n)
+{
+    float out;
+    out = n * 3.93701;
+    return out;
+
+}
+float cmToft(float n)
+{
+    float out;
+    out = n / 30.48;
+    return out;
+}
+float inTomm(float n)
+{
+    float out;
+    out = n * 25.4;
+    return out;
+}
+float inTocm(float n)
+{
+    float out;
+    out = n * 2.54;
+    return out;
+
+}
+float inToin(float n)
+{
+    float out;
+    out = n * 1;
+    return n;
+}
+float inToft(float n)
+{
+    float out;
+    out = n * 0.0833;
+    return out;
+}
+float ftTomm(float n)
+{
+    float out;
+    out = n * 304.8;
+    return out;
+}
+float ftTocm(float n)
+{
+    float out;
+    out = n * 30.48;
+    return out;
+}
+float ftToin(float n)
+{
+    float out;
+    out = n * 12;
+    return out;
+
+}
+
+float ftToft(float n)
+{
+    float out;
+    out = n * 1;
+    return n;
+}
